fad362868af415f991d5c0f78d5b4466d1917634
Mariusz Kryński <mrk@sed.pl>
2022-01-24 10:22:39 +0100

    fix docs-rs building, bump version

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "docs-rs")]
+fn main() {} // Skip the script when the doc is building
+
+#[cfg(not(feature = "docs-rs"))]
 fn main() {
     cc::Build::new()
         .compiler("mos-a800xl-clang")
